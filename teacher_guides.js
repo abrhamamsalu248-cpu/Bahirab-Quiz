ee54c57426dacdb56f7b8a9e4a0a869417ee5c2d
abrhamamsalu248-cpu <abrhamamsalu248@gmail.com>
2026-08-29 19:20:29 +0300

    Create teacher_guides.js with guide information
    Added an array of teacher guides with details.

---
+++ teacher_guides.js
@@ -0,0 +1,18 @@
+const teacherGuides = [
+  {
+    id: "guide_economics",
+    title: "Economics Teacher Guide",
+    category: "Teacher Guide",
+    fileKey: "guide_economics",
+    price: 100,
+    link: "https://t.me/bahirabquiz/31"
+  },
+  {
+    id: "guide_mathematics",
+    title: "Mathematics Teacher Guide",
+    category: "Teacher Guide",
+    fileKey: "guide_mathematics",
+    price: 100,
+    link: "https://t.me/bahirabquiz/32"
+  }
+];
