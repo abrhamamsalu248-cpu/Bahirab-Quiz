277ee6390f9c7a19b8f130d36ef19ad17f6132ad
abrhamamsalu248-cpu <abrhamamsalu248@gmail.com>
2026-08-30 22:01:09 +0300

    Create notes_data.js

---
+++ notes_data.js
@@ -0,0 +1,18 @@
+window.courseNotesData = [
+    {
+        id: "civics_ch1",
+        category: "civics",
+        course: "⚖️ Moral & Civics",
+        title: "Civics Chapter 1 (Note)",
+        desc: "Bahir Dar University • Course Note",
+        pdfFile: "civics_ch1.pdf"
+    },
+    {
+        id: "entrepreneurship_ch1",
+        category: "entrepreneurship",
+        course: "💼 Entrepreneurship",
+        title: "Entrepreneurship Chapter 1 (Note)",
+        desc: "Bahir Dar University • Course Note",
+        pdfFile: "Entrepreneurship_Chapter_1.pdf"
+    }
+];
