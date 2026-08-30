dac93078bcd1034c3c347f9af68bc4b2f387b9da
abrhamamsalu248-cpu <abrhamamsalu248@gmail.com>
2026-08-30 22:05:39 +0300

    Update notes_data.js

--- notes_data.js
+++ notes_data.js
@@ -4,15 +4,17 @@ window.courseNotesData = [
         category: "civics",
         course: "⚖️ Moral & Civics",
         title: "Civics Chapter 1 (Note)",
-        desc: "Bahir Dar University • Course Note",
-        pdfFile: "civics_ch1.pdf"
+        desc: "Course Note",
+        pdfFile: "civics_ch1.pdf",
+        cost: 500
     },
     {
         id: "entrepreneurship_ch1",
         category: "entrepreneurship",
         course: "💼 Entrepreneurship",
         title: "Entrepreneurship Chapter 1 (Note)",
-        desc: "Bahir Dar University • Course Note",
-        pdfFile: "Entrepreneurship_Chapter_1.pdf"
+        desc: "Course Note",
+        pdfFile: "Entrepreneurship_Chapter_1.pdf",
+        cost: 500
     }
 ];
